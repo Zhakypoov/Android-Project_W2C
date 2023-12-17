4118c98beebe186556079248e6c6b2ceb826356d
Bekzat-Chauvbayev <kazna51k@gmail.com>
2023-12-17 16:28:53 +0600

    login page created

---
+++ app/src/main/java/com/example/w2c/screens/LoginScreen.kt
@@ -0,0 +1,13 @@
+package com.example.w2c.screens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.w2c.R
+
+class LoginScreen (): AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.login_page)
+    }
+}
